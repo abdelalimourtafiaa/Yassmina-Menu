8fbe5824bcc4b769ac63ca804fa4daf111b37fbb
Abdelali Mourtafiaa <abdelalimourtafiaa@gmail.com>
2023-05-18 01:02:30 +0100

    fetch product by categories

--- lib/model/ProduitModel.dart
+++ lib/model/ProduitModel.dart
@@ -4,13 +4,16 @@ class ProduitModel {
   String? description;
   double? prix;
   String? image ;
+  int? id_category;
+
 
   ProduitModel(
       {required this.id_produit,
         required this.name,
         required this.description,
         required this.prix,
-        required this.image});
+        required this.image,
+        required this.id_category,});
 
   factory ProduitModel.fromJson(Map<String, dynamic> json) {
     return ProduitModel(
@@ -19,6 +22,7 @@ class ProduitModel {
       description: json['description'] ,
       prix: double.parse(json['prix'].toString()) ,
       image: json['image'] ,
+      id_category: json['id_category'],
     );
   }
 }
